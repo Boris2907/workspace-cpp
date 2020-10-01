c40a209b92633c83ea0651389b140071fbb5fab9
Boris2907 <boris290700@inbox.lv>
2020-10-01 12:34:25 +0300

    Updating double output

--- ex01-periodic/Periodic.cpp
+++ ex01-periodic/Periodic.cpp
@@ -8,117 +8,119 @@
 #include <bits/stdc++.h>
 #include "Periodic.h"
 
-using namespace std; 
+using namespace std;
 
 int period = 0;
 
-void findPeriodic( char word[], int wordLength, int prefix[]) { 
-    int i = 1, c=0, d=0, f=0, length=0, truecounter=0;; 
-    int zeroes = 0;
-    string newArray = "";
-    prefix[0] = 0; 
+void findPeriodic(char word[], int wordLength, int prefix[])
+{
+  int i = 1, c = 0, d = 0, f = 0, length = 0, truecounter = 0;
+  ;
+  int zeroes = 0;
+  string newArray = "";
+  prefix[0] = 0;
 
-    while ( f < wordLength) {
-      if (word[f]=='0') {
-        zeroes++;
-        f++;
-      } 
-      else {
-        break;
+  while (f < wordLength)
+  {
+    if (word[f] == '0')
+    {
+      zeroes++;
+      f++;
+    }
+    else
+    {
+      break;
+    }
+  }
+
+  while (c < wordLength)
+  {
+    if (isdigit(word[c]))
+    {
+      truecounter++;
+      break;
+    }
+    c++;
+  }
+
+  if (truecounter > 0)
+  {
+    while (d < wordLength)
+    {
+      if (isupper(word[d]))
+      {
+        word[d] = tolower(word[d]);
+      }
+      d++;
+    }
+    truecounter = 0;
+  }
+
+  while (i < wordLength)
+  {
+    if (word[i] == word[length])
+    {
+      length++;
+      prefix[i] = length;
+      i++;
+    }
+    else
+    {
+      if (length != 0)
+      {
+        length = prefix[length - 1];
+      }
+      else
+      {
+        prefix[i] = 0;
+        i++;
       }
     }
-
-    while (c < wordLength) {
-      if (isdigit(word[c])) {
-        truecounter++;
-        break;
-      }
-      c++;
-    }
-
-    if (truecounter > 0) {
-      while (d < wordLength) {
-        if (isupper(word[d])) {
-          word[d]=tolower(word[d]);
-        }
-        d++;
-      }
-      truecounter=0;
-    }
-
-    
-    while (i < wordLength) { 
-       if (word[i] == word[length]) { 
-           length++; 
-           prefix[i] = length; 
-           i++;   
-       } 
-       else { 
-          if (length != 0) { 
-             length = prefix[length-1]; 
-          } 
-          else { 
-             prefix[i] = 0; 
-             i++; 
-          } 
-       } 
-    } 
+  }
   period = i - length;
-} 
-  
-bool isPeriodic(char str[]) { 
-    int prefix[strlen(str)]; 
-
-    findPeriodic(str, strlen(str), prefix); 
-    int length = prefix[strlen(str)-1]; 
-
-    if (strlen(str)%(strlen(str)-length) == 0 && length > 0) {
-      return true;
-    }
-    else {
-      return false;
-    }
 }
 
-string zeroesDeleter(char input[]) {
-int i = 0;
- std::vector<char> testVector;
+bool isPeriodic(char str[])
+{
+  int prefix[strlen(str)];
 
- while ( i < strlen(input)) {
-   testVector.push_back(input[i]);
-   i++;
-   
- }
+  findPeriodic(str, strlen(str), prefix);
+  int length = prefix[strlen(str) - 1];
 
-//  for (int j = 0 ; j < testVector.size(); j++) {
-//   cout << testVector[i];
-//  }
+  if (strlen(str) % (strlen(str) - length) == 0 && length > 0)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
 
+string zeroesDeleter(char input[])
+{
+  int i = 0;
+  std::vector<char> testVector;
 
-  while(testVector[0] == '0'){
+  while (i < strlen(input))
+  {
+    testVector.push_back(input[i]);
+    i++;
+  }
+
+  while (testVector[0] == '0')
+  {
+    testVector.erase(testVector.begin());
+  }
+  std::reverse(testVector.begin(), testVector.end());
+
+  while (testVector[0] == '0')
+  {
     testVector.erase(testVector.begin());
   }
 
-	// for (auto x: testVector) {
-	// 	cout << x << endl;;
-	// }
-std::reverse(testVector.begin(), testVector.end());
+  std::reverse(testVector.begin(), testVector.end());
 
-while(testVector[0] == '0'){
-    testVector.erase(testVector.begin());
-  }
-
-std::reverse(testVector.begin(), testVector.end());
-
-std::string s(testVector.begin(),testVector.end());
-return s;
-// char p[10000]; 
-//   int i; 
-//   for (i = 0; i < sizeof(p); i++) { 
-//       p[i] = s[i]; 
-//   }
-//   i++;
-//   p[i] = '\0';
-//   cout << p << " right here";
-//   return p;
+  std::string s(testVector.begin(), testVector.end());
+  return s;
 }
--- ex01-periodic/PeriodicMain.cpp
+++ ex01-periodic/PeriodicMain.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "Periodic.h"
+#include <sstream>
+#include <string>
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -7,19 +9,29 @@ using namespace std;
 
 int main(){
   extern int period;
+  std::string inputLine;
   char mode[10000], inputString[10000];
   cin >> mode;
-  while(!cin.eof()){
-    cin >> inputString;
+
+  cin.ignore(10000, '\n');
+  while(getline(cin, inputLine)){
+    istringstream sstr(inputLine);
+    if (sstr.peek() == '#') {
+      continue;
+    } else {
+    sstr >> inputString;
     string someStr = zeroesDeleter(inputString);
     int n = someStr.length();
     char char_array[n+1];
     strcpy(char_array, someStr.c_str());
+
     if (isPeriodic(char_array)) {
         cout  << char_array  << " " << period << endl;
       }    
       else { 
         cout << char_array  << " 0" << endl; 
       }
+
+    }
   }
 }
